d2114ae977974649329aedcde4a0cde260a251d9
IbonifaceI <boniface3791@gmail.com>
2026-02-06 15:38:54 +0300

    Delete src/types directory

--- src/types/PlayerNames.ts
+++
@@ -1,5 +0,0 @@
-// Интерфейс для хранения имен игроков
-export type PlayerNames = {
-  playerOne: string;
-  playerTwo: string;
-};
--- src/types/SquareProps.ts
+++
@@ -1,5 +0,0 @@
-// Тип props для компонента Square
-export interface SquareProps {
-  value: string | null;
-  onClick: () => void;
-}
